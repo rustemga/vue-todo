76e099f515fadec8ec6cdcb1cc3079060459b4b2
Rustem.Garaev <Tel52093547!@>
2020-10-28 17:29:44 +0300

    First

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Todo</h1>
+    <TodoList
+      v-bind:todos="todos"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TodoList from "./components/TodoList"
 export default {
   name: 'App',
+  data(){
+    return{
+      todos: [
+        {id:1, title: "Купить хлеб", completed: false},
+        {id:2, title: "Купить молоко", completed: false},
+        {id:3, title: "Купить масло", completed: false},
+      ]
+    }
+  },
   components: {
-    HelloWorld
+    TodoList
   }
 }
 </script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <ul>
+      <li>
+        <TodoItem
+        v-for="todo in todos"
+        v-bind:key="todo"
+        v-bind:todo = "todo"
+        />
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+  import TodoItem from "@/components/TodoItem";
+  export default {
+    components: {TodoItem},
+    comments: {
+      TodoItem
+    },
+    props:[
+        'todos'
+    ]
+  }
+</script>
